8d1268ce03ce7e94e329cf5d4e78189775bf537b
Jake Johnston <jakejohnston618@gmail.com>
2023-03-22 09:40:04 -0500

    Temperature conversion worked

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is the temperature in F you wanna convert?");
+            double fTemp = Convert.ToDouble(Console.ReadLine());
+            var convertedToCel = TempConverter.FahrenheitToCelsius(fTemp);
+
+            Console.WriteLine($"That equals {convertedToCel} degrees celsius\n");
+
+            Console.WriteLine("What is the temperature in C that you want to convert?");
+            double celTemp = Convert.ToDouble(Console.ReadLine());
+            var convertedToFar = TempConverter.CelsiusToFahrenheit(celTemp);
+
+            Console.WriteLine($"That equals {convertedToFar} degrees fahrenheit\n");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            var celTemp = (fTemp - 32) * (5.0 / 9.0);
+            return celTemp;
+        }
+        public static double CelsiusToFahrenheit(double celTemp)
+        {
+            return celTemp * (9.0 / 5.0) + 32;
+        }
+    }
+   
+}
